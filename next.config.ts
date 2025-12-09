5baaf565d592ebb12c9202c2f488b77233ab6b3b
lars0407 <74684066+lars0407@users.noreply.github.com>
2025-12-09 22:40:41 +0100

    234234

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,5 @@
 import type { NextConfig } from 'next';
 
-const nextConfig: NextConfig = {
-  experimental: {
-    clientSegmentCache: true,
-    nodeMiddleware: true
-  }
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
